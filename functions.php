9cb8578bce1e0d42d53ba4364f76e9c9a51f6be0
Matthew Dunbar <matthewdunbar11@gmail.com>
2015-05-28 06:21:02 -0400

    base markup

--- functions.php
+++ functions.php
@@ -1,19 +1,29 @@
 <?php
 
-class Bootstrap_Starter {
+require_once('wp_bootstrap_navwalker.php');
+
+class The_Church {
 
 	function __construct() {
-
+		add_action( 'init', array($this, 'init' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
 
 	}
 
+	function init() {
+		register_nav_menu( 'primary', 'Primary' );
+	}
+
 	function wp_enqueue_scripts() {
-		wp_enqueue_script( 'test', get_stylesheet_directory_uri() . '/test.js' );
-		wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' );
-		wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' );
 		wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array( 'jquery' ), '3.3.4', true );
+		
+		wp_enqueue_style( 'quicksand', 'http://fonts.googleapis.com/css?family=Quicksand:400,700' );		
+		wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' );
+		wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' );
+		
+		wp_enqueue_style( 'typography', get_stylesheet_directory_uri() . '/css/typography.css' );
+		wp_enqueue_style( 'color-settings', get_stylesheet_directory_uri() . '/css/colors.css' );
 	}
 
 }
-new Bootstrap_Starter();
+new The_Church();
--- header.php
+++ header.php
@@ -9,3 +9,38 @@
 	</head>
 
 	<body>
+    <div class="primary full-width">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 text-center">
+            <h1 class="skinny"><?php print get_bloginfo('name'); ?></h1>
+          </div>
+          <div class="col-xs-12">
+            <nav class="navbar navbar-default" role="navigation">
+              <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>            
+                <?php wp_nav_menu(array(
+                  'theme_location' => 'primary',
+                  'container'         => 'div',
+                  'container_class'   => 'collapse navbar-collapse',
+                  'container_id'      => 'bs-example-navbar-collapse-1',
+                  'menu_class'        => 'nav navbar-nav',
+                  'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                  'walker'            => new wp_bootstrap_navwalker()
+                )); ?>
+              </div>
+            </nav>
+          </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+        
